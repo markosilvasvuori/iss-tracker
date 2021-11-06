0b055f75f83e6ee5340dbf21e150730465b67b9b
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2021-11-06 16:18:44 +0200

    Added mapboxgl import and mapboxgl.workerClass...

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -6,6 +6,13 @@ import MapControls from './MapControls';
 import IssMarker from '../ISS/IssMarker';
 import classes from './Map.module.css';
 
+import mapboxgl from 'mapbox-gl';
+// The following is required to stop "npm build" from transpiling mapbox code.
+// notice the exclamation point in the import.
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+
 const Map = () => {
     const [issData, setIssData] = useState({
         latitude: 0,
