f7d057919c27260aa0e06b47ac628432548df0c8
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2021-11-06 15:27:30 +0200

    Changed 'Follow ISS' button to 'Center ISS' checkbox

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -4,7 +4,6 @@ import ReactMapGL from 'react-map-gl';
 import LoadingIcon from '../UI/LoadingIcon';
 import MapControls from './MapControls';
 import IssMarker from '../ISS/IssMarker';
-import Button from '../UI/Button';
 import classes from './Map.module.css';
 
 const Map = () => {
@@ -22,10 +21,7 @@ const Map = () => {
     const [settings, setSettings] = useState({
         dragPan: false,
         dragRotate: false,
-        scrollZoom: false,
-        touchZoom: false,
         touchRotate: false,
-        doubleClickZoom: false,
         keyboard: false
     });
     const [isFollowingIss, setIsFollowingIss] = useState(true);
@@ -60,10 +56,7 @@ const Map = () => {
             setSettings({
                 dragPan: false,
                 dragRotate: false,
-                scrollZoom: false,
-                touchZoom: false,
                 touchRotate: false,
-                doubleClickZoom: false,
                 keyboard: false
             }); 
             setViewport({
@@ -77,10 +70,7 @@ const Map = () => {
             setSettings({
                 dragPan: true,
                 dragRotate: true,
-                scrollZoom: true,
-                touchZoom: true,
                 touchRotate: true,
-                doubleClickZoom: true,
                 keyboard: true
             });        
         }
@@ -90,35 +80,21 @@ const Map = () => {
         setShowControls(!showControls);
     };
 
-    const followIssHandler = () => {
+    const followIssHandler = (  ) => {
         setIsFollowingIss(!isFollowingIss);
     };
 
     return (
         <div className={classes.map}>
             {isLoading && <LoadingIcon />}
-            {!isLoading && !showControls &&
-                <Button 
-                    className={classes["controls-button"]} 
-                    onClick={showControlsHandler}
-                >
-                    Controls
-                </Button>
-            }
-            {showControls &&
-                <MapControls 
-                    hideControls={showControlsHandler}
-                    onFollow={followIssHandler} 
-                    isFollowing={isFollowingIss} 
-                />
-            }
+            {!isLoading && <MapControls onClick={followIssHandler} /> }
             {!isLoading &&
                 <ReactMapGL
+                    {...viewport}
+                    {...settings}
                     mapStyle={'mapbox://styles/markosilvasvuori/ckvjje8ie084v14ou6z0e575x'}
                     onViewportChange={nextViewport => setViewport(nextViewport)}
                     mapboxApiAccessToken={API_KEY}
-                    {...viewport}
-                    {...settings}
                 >
                     <IssMarker 
                         longitude={issData.longitude} 
